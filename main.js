dd9c92dc9fe2fd8c880406a097301c301de028f6
Gabriel Corrêa <127027465+gabrielc-neto@users.noreply.github.com>
2025-04-17 18:31:12 -0300

    Create main.js
    Inserção de dados primários

---
+++ main.js
@@ -0,0 +1,28 @@
+let atletas = [
+    {
+      nome: "Cesar Abascal",
+      notas: [10, 9.34, 8.42, 10, 7.88]
+    },
+    {
+      nome: "Fernando Puntel",
+      notas:  [8, 10, 10, 7, 9.33]
+    },
+    {
+      nome: "Daiane Jelinsky",
+      notas: [7, 10, 9.5, 9.5, 8]
+    },
+    {
+      nome: "Bruno Castro",
+      notas: [10, 10, 10, 9, 9.5]
+    }
+   ];
+function recebeMatriz(matriz){
+    let media = 0;
+   //for (let i = 0; i < matriz.length; i++){
+    //média sem consinderar a maior e menor nota
+    //media = 
+    //Math.max()
+    //Math.min()
+//}
+    return
+}
